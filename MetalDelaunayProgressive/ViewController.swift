976b751f1d0b6f238d4d615bdd9377e0a4df9a49
plutovman <vladimir@vladimirs-imac.local>
2017-05-31 21:26:26 -0700

    Initial Commit

---
+++ MetalDelaunayProgressive/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MetalDelaunayProgressive
+//
+//  Created by vladimir sierra on 5/31/17.
+//  Copyright © 2017 vladimir sierra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
